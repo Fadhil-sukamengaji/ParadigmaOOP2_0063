f8c0c235f9b9edba348b106b3073ec63aadc46e0
Fadhil-sukamengaji <mudzakifadhil@gmail.com>
2026-05-18 17:35:44 +0700

    int main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,15 @@ public:
         return saklarNo[i];
     }
 };
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "lampu teras rumah");
+    lampuRumah.setSaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setSaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setSaklarNo(3, "lampu dapur");
+
+
+    return 0;
+}
