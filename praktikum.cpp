37da32c71919114e68b7eaf503de32a3627e8770
Fadhil-sukamengaji <mudzakifadhil@gmail.com>
2026-05-18 19:53:06 +0700

    class rekening konvensional

--- praktikum.cpp
+++ praktikum.cpp
@@ -31,3 +31,12 @@ public:
     }
 };
 
+class RekeningKonvensional : public RekeningBank {
+public:
+    RekeningKonvensional(string n, double s) : RekeningBank(n, s) {}
+    
+    void potongAdmin() override {
+        saldo -= 15000;
+    }
+};
+
